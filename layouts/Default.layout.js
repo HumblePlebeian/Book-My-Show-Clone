c66c6e6d167773ece965443dcd7750c35356039c
Vibhuti Saha <71592155+HumblePlebeian@users.noreply.github.com>
2021-10-20 14:22:43 +0530

    Create Default.layout.js

---
+++ layouts/Default.layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//components
+import Navbar from "../components/Navbar/navbar.component";
+import HeroCarousal from "../components/HeroCarousal/HeroCarousal.component.js";
+
+const DefaultLayout = (props) => {
+  return (
+    <>
+    <Navbar />
+    <HeroCarousal />
+    {props.children}
+    </>
+  );
+};
+
+export default DefaultLayout;
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Se
